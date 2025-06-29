7510807fdda6f85f6d67a03544fc9b3b9f0eaad1
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-06-30 00:57:14 +0100

    updatecurso

--- golang/pacotes/main.go
+++ golang/pacotes/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
 	auxiliar.Escrever()
+	
+
+	err := checkmail.ValidateFormat("prod@gmail.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+	
 }
