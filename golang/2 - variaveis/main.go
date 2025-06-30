3656fb8a926f35b51da18d0edbd4baa0e82bbac6
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-06-30 01:08:10 +0100

    update

--- golang/2 - variaveis/main.go
+++ golang/2 - variaveis/main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
+	var variavel1 string = "Olá 1"
+	variavel2 := "Olá 2"
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "Olá 3"
+		variavel4 string = "Olá 4"
+	)
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Olá 5", "Olá 6"
+	fmt.Println(variavel5, variavel6)
+
+	const constante string = "Constante"
+	fmt.Println(constante)
+
+	const (
+	constante1 string = "Constantezinha"
+	constante2 string = "Constantationzinha"
+	)
 }
