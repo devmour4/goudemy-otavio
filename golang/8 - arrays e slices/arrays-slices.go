e30f580e187c9f6b49ed9fdf63b706ccd1b86dbc
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-07-09 01:20:31 +0100

    ponteiros.go

---
+++ golang/8 - arrays e slices/arrays-slices.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices!")
+
+	//criando um array de 5 posições
+	var array1 [5]string
+	//preenchendo as posições
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	//criando um array de 5 posições e "preenchendo"
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	//criando um array de forma elegante e sem especificar o tamanho
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+	
+	//criando um slice com 5 posições
+	slice := []int{11, 12, 13, 14, 15}
+	fmt.Println(slice)
+	
+	//incrementando mais uma posição no slice
+	slice = append(slice, 16)
+	
+	fmt.Println(slice)
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+}
