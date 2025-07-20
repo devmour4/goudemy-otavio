4408e155baa5331623f265b2c47b434b844b3ef4
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-07-21 00:05:09 +0100

    Create switch.go

---
+++ golang/12 - switch/switch.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+func switchCase(numero int)	string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-Feira"
+	case 3:
+		return "Terça-Feira"
+	case 4:
+		return "Quarta-Feira"
+	case 5:
+		return "Quinta-Feira"
+	case 6:
+		return "Sexta-Feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número Inválido!"
+
+	}
+}
+func switchCase2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda-Feira"
+	case numero == 3:
+		return "Terça-Feira"
+	case numero == 4:
+		return "Quarta-Feira"
+	case numero == 5:
+		return "Quinta-Feira"
+	case numero == 6:
+		return "Sexta-Feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número Inválido!"
+
+	}
+}
+
+
+func main() {
+	fmt.Println("***--Switch-Case--***")
+
+	dia := switchCase(5)
+	dia2 := switchCase2(4)
+	fmt.Println(dia)
+	fmt.Println(dia2)
+}
